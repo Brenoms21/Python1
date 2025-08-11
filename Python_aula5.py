d001861fdd4d1f7b146155607f8357a3e24ad365
labsfiap <breno.martins7573@gmail.com>
2025-08-11 20:51:56 -0300

    aula de lista

---
+++ Python_aula5.py
@@ -0,0 +1,51 @@
+'''L = []
+while True:
+    num = int(input("Digite um número(0 sai):"))
+    if num == 0:
+    break
+    L.append(num)
+x = 0
+while x < len(L):
+    print(L[x])
+    x += 1
+nomes_alunos = []
+while True:
+    nome = input("Nome (digite fim para sair): ")
+    if nome == "fim":
+        break
+    nomes_alunos.append(nome)
+
+nomes_aluno.sort()
+x = 0
+whiles x < len(nomes_alunos):
+print(nomes_alunos[x])
+x += 1
+
+nomes_alunos = []
+nomes_alunos = nomes_alunos + ["Breno"]
+print(nomes_alunos)
+nomes_alunos += ["Cristiano"]
+print(nomes_alunos)
+nomes_alunos += ["Giovanna" , "Leonardo"]
+print(nomes_alunos)
+
+nomes_alunos = ["Michele" , "Gustavo" , "Giovanna" , "Vinicius"]
+nomes_alunos_novos = []
+while True:
+    aluno_novo = input("Nome (fim para sair): ")
+    if aluno_novo == "fim":
+        break
+    nomes_alunos_novos.append(aluno_novo)
+
+#nova_lista = nomes_alunos + nomes_alunos_novos
+#print(nova_lista)
+nova_lista = []
+nova_lista.extend(nomes_alunos + nomes_alunos_novos)
+print(nova lista)
+
+nova_lista = ['Michele', 'Gustavo', 'Giovanna', ]
+del nova_lista[] '''
+
+
+
+
